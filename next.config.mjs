167d7366bc12985ba4c75f0c3af6f21fc0fafc89
Lee, Raymond <ralee@webmd.net>
2024-01-23 12:24:53 -0500

    PPV-7: Routing setup

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const nextConfig = {
+
     sassOptions: {
         includePaths: [path.join(__dirname, 'src', 'app', 'styles')],
     },
