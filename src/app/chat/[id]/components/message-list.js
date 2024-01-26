90036bbbc78032f5dc395a1cc3f7ccbfc5d21227
Raymond Lee <>
2024-01-26 12:09:10 -0500

    PPV-8: ui updates

--- src/app/chat/[id]/components/message-list.js
+++ src/app/chat/[id]/components/message-list.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import CodeIcon from '@mui/icons-material/Code';
+import OpenAiIcon from '@/app/components/icons/openai-icon';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import StringToHtmlConverter from '@/app/components/string-to-html-converter';
 
 const MessageList = () => {
   const messages = useSelector((state) => state.chat.messages);
@@ -11,11 +12,11 @@ const MessageList = () => {
     <div className="message-list" data-component="MessageList">
       {
         messages.map((message, index) => {
-          const Icon = message.role === "user" ? AccountCircleIcon : CodeIcon;
+          const Icon = message.role === "user" ? AccountCircleIcon : OpenAiIcon;
           return (
             <div key={index} className="message-row" data-timestamp={message.timestamp} data-role={message.role}>
               <div className="role"><Icon /></div>
-              <div className="content">{message.content}</div>
+              <div className="content"><StringToHtmlConverter content={message.content} /></div>
             </div>
           )
         })
